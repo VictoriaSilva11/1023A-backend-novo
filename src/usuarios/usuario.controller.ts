f28cda5ad810e5cc3981e4e2f42d5b719bd0f30a
VictoriaSilva11 <victoria.silva7@estudante.ifms.edu.br>
2025-09-23 08:21:00 -0400

    corrigindo erros

--- src/usuarios/usuario.controller.ts
+++ src/usuarios/usuario.controller.ts
@@ -1,15 +1,15 @@
-import { Request, Response } from 'express';
-import { db } from '../database/banco-mongo.js';
-class ProdutoController {
-    async adicionar(req:Request, res:Response) {
-        const {nome,preco,descricao,urlfoto} = req.body;
-        const produto = {nome,preco,descricao,urlfoto};
-        const resposta = await db.collection('produtos').insertOne(produto);
-        res.status(201).json({...produto, _id: resposta.insertedId});
+import { Request, Response } from "express";
+import { db } from "../database/banco-mongo.js";
+class UsuarioController {
+    async adicionar(req: Request, res: Response) {
+        const estudante = req.body
+        const resultado = await db.collection('estudantes')
+            .insertOne(estudante)
+        res.status(201).json({ ...estudante, _id: resultado.insertedId })
     }
-    async listar(req:Request, res:Response) {
-        const produtos = await db.collection('produtos').find().toArray();
-        res.status(200).json(produtos);
+    async listar(req: Request, res: Response) {
+        const estudantes = await db.collection('estudantes').find().toArray();
+        res.status(200).json(estudantes);
     }
 }
-export default new ProdutoController();
+export default new UsuarioController();
