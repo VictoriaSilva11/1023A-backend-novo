88f61579447e7cd810fa5c76057fc8859f2c2b08
VictoriaSilva11 <victoria.silva7@estudante.ifms.edu.br>
2025-10-21 11:50:39 -0400

    continuação do código após a viagem

--- src/index-mongo.ts
+++ src/index-mongo.ts
@@ -1,11 +1,19 @@
 import 'dotenv/config'
 import express from 'express';
-import rotas from './rotas.js';
+import rotasAutenticadas from './rotas/rotas-autenticadas.js';
+import rotasNaoAutenticadas from './rotas/rotas-nao-autenticadas.js';
+import Auth from './middleware/auth.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(rotas);
+
+
+app.use(rotasNaoAutenticadas)
+app.use(Auth)
+app.use(rotasAutenticadas);
 
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
-});                                      
+});
