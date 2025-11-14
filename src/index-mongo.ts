8c612dbd46e6a43cf123c387860ac3a2e754be42
VictoriaSilva11 <victoria.silva7@estudante.ifms.edu.br>
2025-11-14 16:33:10 -0400

    arrumando

--- src/index-mongo.ts
+++ src/index-mongo.ts
@@ -14,8 +14,6 @@ app.use(rotasNaoAutenticadas)
 app.use(Auth)
 app.use(rotasAutenticadas);
 
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
 });
